40e918e2127c0341e66d9470312425aeb3c16ee9
mr5 <u@mr-5.cn>
2017-07-20 10:49:39 +0800

    add omissive files

---
+++ gulpfile.babel.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const path = require('path');
+const babel = require('gulp-babel');
+const fs = require('fs');
+const sourceMaps = require('gulp-sourcemaps');
+
+const paths = {
+  es6: ['./src/**/*.js'],
+  es5: './build',
+  sourceRoot: path.join(__dirname, 'src')
+};
+
+gulp.task('build', () => {
+  gulp.src(['./src/**/*.json', './src/**/*.sql']).pipe(gulp.dest('./build'));
+  return gulp
+    .src(paths.es6)
+    .pipe(sourceMaps.init())
+    .pipe(babel(JSON.parse(fs.readFileSync(`${__dirname}/.babelrc`))))
+    .pipe(sourceMaps.write('.', {
+      includeContent: false,
+      sourceRoot: paths.sourceRoot
+    }))
+    .pipe(gulp.dest(paths.es5));
+});
